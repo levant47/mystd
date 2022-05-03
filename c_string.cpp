c4433fa4ebcde0a6a9a834ad7dc85ecfd7428022
levant47 <levant.ba@gmail.com>
2022-05-03 11:52:46 +0300

    c_string_equal

--- c_string.cpp
+++ c_string.cpp
@@ -10,3 +10,20 @@ u64 get_c_string_length(CStringView source)
     }
     return i;
 }
+
+bool c_string_equal(CStringView left, CStringView right)
+{
+    u64 i = 0;
+    while (true)
+    {
+        if (left[i] != right[i])
+        {
+            return false;
+        }
+        if (left[i] == '\0')
+        {
+            return true;
+        }
+        i++;
+    }
+}
